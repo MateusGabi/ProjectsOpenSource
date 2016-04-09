bfa60bde98aaa77867a7e86a7cf4eafb63004164
Mateus Gabi <mateusgabimoreira@hotmail.com>
2016-04-09 16:15:28 -0400

    v 2.2.4

--- Project Ameda/engineAmeda.js
+++ Project Ameda/engineAmeda.js
@@ -1,6 +1,6 @@
 /*
  
- Versão 2.2.3 
+ Versão 2.2.4 
  
  @author Mateus Gabi    [@matgabi17 on Twitter]
  @author Renan Benatti  [@renanbdias on Twitter]
@@ -8,41 +8,39 @@
  */
 
 //  'a', 'b', 'c' e 'd' são variáveis do tipo 'integer' e recebem valores sorteados;
-//  'a', 'b', 'c' and 'd' are 'integer' variables and receives a randon number;
 
-var a = criarUnico();
-var b = criarUnico();
-var c = criarUnico();
-var d = criarUnico();
+// var a = criarUnico();
+// var b = criarUnico();
+// var c = criarUnico();
+// var d = criarUnico();
 
 //  Valor limite de prints de palavras, ou seja, a Ameda printará entre 0 e 7 palavras;
-//  Number max of words;
-var e = 7;
+var e = 4;
 
 //  Função que retorna um número aleatório
-function criarUnico()
-{
+// function criarUnico()
+// {
 
-    var sorteados = [];
-    var valorMaximo = 8;
+//     var sorteados = [];
+//     var valorMaximo = 8;
 
-    if (sorteados.length == valorMaximo)
-    {
-        if (confirm('#'))
-            sorteados = [];
-        else
-            return;
-    }
+//     if (sorteados.length == valorMaximo)
+//     {
+//         if (confirm('#'))
+//             sorteados = [];
+//         else
+//             return;
+//     }
 
-    var sugestao = Math.ceil(Math.random() * valorMaximo);
+//     var sugestao = Math.ceil(Math.random() * valorMaximo);
 
-    while (sorteados.indexOf(sugestao) >= 0)
-        sugestao = Math.ceil(Math.random() * valorMaximo);
+//     while (sorteados.indexOf(sugestao) >= 0)
+//         sugestao = Math.ceil(Math.random() * valorMaximo);
 
-    sorteados.push(sugestao);
+//     sorteados.push(sugestao);
 
-    return sugestao;
-}
+//     return sugestao;
+// }
 
 var w = [
     //[Susbstantivo Comum], [Substantivo Próprio], [adjetivo], [advérbio], [verbo]
@@ -70,7 +68,7 @@ var w = [
     "vegano", "Vitória", "verde", "vencedor",
     "Wellington",
     "xadrez", "Xico",
-    "Ygritte", 
+    "Ygritte",
     "zebra",
     "abano",
     "boliche",
@@ -84,22 +82,27 @@ var w = [
     "jato",
     "liquidificar",
     "namorar"
-    ];
+];
 
-function printarFrase()
-{
+function printarFrase() {
 
     var retorno = "";
 
-    for (i = 1; i <= e; i++)
-    {
+    var f = -1;
+
+    for (i = 1; i <= e; i++) {
 
         //var w[] = getWords();
 
         // função para encontrar elemento no vetor
-        // f será o indice
-        
-        var f = i * a - b + i * c - d;
+        // f será o indice do vetor w
+
+        var _f = parseInt(Math.random() * w.length);
+
+        if (f == _f)
+            i--;
+        else
+            f = _f;
 
         // if (f > w.length || f < 0) {
         //     do nothing
@@ -108,27 +111,26 @@ function printarFrase()
         // }
 
         if (f > 0 && f < w.length)
-            retorno += " " +w[f]
+            retorno += " " + w[f]
 
     }
 
-    return document.getElementById('frase').innerHTML = retorno;
+    return document.getElementById('frase').innerHTML = "<input id=\"ph\" class=\"form-control input-lg\" type=\"text\" placeholder=\"Carregando...\" value=\"" + retorno + "\" readonly>";
 }
 
 // Resultado Inteiro em Trilhões
 
-function printarWordsStatus()
-{    
+function printarWordsStatus() {
     var p = 0;
 
     for (var i = 1; i <= e; i++)
         p += Math.pow(w.length, i);
 
-    p = parseInt(p / Math.pow(10, 12));
+    p = parseInt(p / Math.pow(10, 6));
 
     // p = parseInt(p);
 
-    return document.getElementById("statusWord").innerHTML = w.length + " words available and " + p + "T+ possibilities!";
+    return document.getElementById("statusWord").innerHTML = "Nós temos " + w.length + " palavras disponíveis em " + p + " milhões de combinações distintas!";
 }
 
 // We don't need it
